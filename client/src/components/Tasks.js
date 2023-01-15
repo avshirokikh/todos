47289ed6cacf0dc0f07aa44a522e928589f9bd2e
avshirokikh <maxwide@shirokikh.ru>
2023-01-15 09:14:57 +0500

    23-01-15 09:14:57

--- client/src/components/Tasks.js
+++ client/src/components/Tasks.js
@@ -21,13 +21,15 @@ const Tasks = observer(({store}) => {
         <div>
             <TaskEditor store={store}>
             </TaskEditor>
-            <div className="container p-0 my-3 row" style={{"paddingLeft":"0px"}}>
-                <div className="col-sm-9" style={{"paddingLeft":"0px"}}>
-                    <button onClick={() => store.loadTasks(1)} className={` ${active(1)} tasks-type`}>Мои задачи</button>
-                    <button onClick={() => store.loadTasks(2)} className={` ${active(2)} tasks-type`}>Задачи подчиненных</button>
-                    <button onClick={() => store.loadTasks(3)} className={` ${active(3)} tasks-type`}>Все задачи</button>
+            <div className="container p-0 my-0 row" style={{"paddingLeft":"0px"}}>
+                <div className="col-9 my-0 ps-0" >
+                    <button onClick={() => store.loadTasks(1)} className={` ${active(1)} tasks-type pt-0`}>Мои задачи</button>
+                    <button onClick={() => store.loadTasks(2)} className={` ${active(2)} tasks-type pt-0`}>Задачи подчиненных</button>
+                    <button onClick={() => store.loadTasks(3)} className={` ${active(3)} tasks-type pt-0`}>Все задачи</button>
                 </div>
-                <button className="col-sm-3" onClick={() => store.loadTaskEditorData(-1)}>Создать задачу</button>
+                <div className="col-3 p-0 my-0 text-end">
+                    <button className="pt-0 my-0 btn btn-primary" onClick={() => store.loadTaskEditorData(-1)}>Создать задачу</button>
+                </div>
             </div>
             {store.tasks.map((item) => <Task data={item} key={item.id} store={store}/>)}
         </div>)
